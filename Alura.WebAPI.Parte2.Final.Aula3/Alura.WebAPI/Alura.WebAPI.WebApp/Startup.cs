c8a3bbb470727d771f4db24984e60fa974dba2f3
Bruno Mayerle Leite <brleite@gmail.com>
2021-08-30 14:04:27 -0300

    Início da parte 2 e alteração de nomes da parte 1

---
+++ Alura.WebAPI.Parte2.Final.Aula3/Alura.WebAPI/Alura.WebAPI.WebApp/Startup.cs
@@ -0,0 +1,68 @@
+﻿using Alura.ListaLeitura.Seguranca;
+using Alura.ListaLeitura.HttpClients;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Alura.WebAPI.WebApp.Formatters;
+using System;
+
+namespace Alura.ListaLeitura.WebApp
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AuthDbContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("AuthDB"));
+            });
+
+            services.AddIdentity<Usuario, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            }).AddEntityFrameworkStores<AuthDbContext>();
+
+            services.ConfigureApplicationCookie(options => {
+                options.LoginPath = "/Usuario/Login";
+            });
+
+            services.AddHttpClient<LivroApiClient>(client => {
+                client.BaseAddress = new Uri("http://localhost:6000/api/");
+            });
+
+            services.AddMvc(options => {
+                options.OutputFormatters.Add(new LivroCsvFormatter());
+            }).AddXmlSerializerFormatters();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
